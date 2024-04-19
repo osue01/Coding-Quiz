15bf10d8156ecd6efa8ae9aa77ea00c184563168
Olivia <osue01@gmail.com>
2024-04-18 20:19:20 -1000

    Attempts to start quiz :(

--- script.js
+++ script.js
@@ -1,60 +1,310 @@
-//Create questions array
-const questions = [
-    "Question 1: What is not one of the fundamentals of Javascript?",
-    "Question 2: Which HTML element do we put the Javascript inside of?",
-    "Question 3: How do you create a function in Javascript"
-]
+// script.js
 
-const startTestBtn = document.getElementById('startTestBtn');
-const questionContainer = document.getElementById('questionContainer');
+let questions = [
+	{
+		prompt: `Which HTML element do we put the Javascript inside of?`,
+		options: [
+			"<javascript>",
+			"<js>",
+			"<script>",
+			"<scripting>",
+		],
+		answer: "<script>",
+	},
+
+	{
+		prompt: `How do you create a function in Javascript`,
+		options: [
+			"call myFunction()",
+			"myFunction()",
+			"call function myFunction",
+			"Call.myFunction",
+		],
+		answer: "myFunction()",
+	},
+
+	{
+		prompt: `What is the correct shorthand for Javascript?`,
+		options: [
+			"js",
+			"java",
+			"jass",
+			"jeff",
+		],
+		answer: "js",
+	},
+];
+
+// Get Dom Elements
+
+let questionsEl =
+	document.querySelector(
+		"#questions"
+	);
+let timerEl =
+	document.querySelector("#timer");
+let choicesEl =
+	document.querySelector("#options");
+let submitBtn = document.querySelector(
+	"#submit-score"
+);
+let startQuizBtn =
+	document.querySelector("#start");
+let nameEl =
+	document.querySelector("#name");
+let feedbackEl = document.querySelector(
+	"#feedback"
+);
+let reStartBtn =
+	document.querySelector("#restart");
+
+// Quiz's initial state
 let currentQuestionIndex = 0;
+let time = questions.length * 15;
+let timerId;
 
-function showNextQuestion() {
-    if (currentQuestionIndex < questions.length) {
-        questionContainer.textContent = questions[currentQuestionIndex];
-        currentQuestionIndex++;
-    } else {
-        questionContainer.textContent = "End of Quiz";
-    }
+// Start quiz and hide frontpage
+
+function quizStart() {
+	timerId = setInterval(
+		clockTick,
+		1000
+	);
+	timerEl.textContent = time;
+	let landingScreenEl =
+		document.getElementById(
+			"start-screen"
+		);
+	landingScreenEl.setAttribute(
+		"class",
+		"hide"
+	);
+	questionsEl.removeAttribute(
+		"class"
+	);
+	getQuestion();
 }
 
-startTestBtn.addEventListener ('click', () => {
-    showNextQuestion();
-});
+// Loop through array of questions and
+// Answers and create list with buttons
+function getQuestion() {
+	let currentQuestion =
+		questions[currentQuestionIndex];
+	let promptEl =
+		document.getElementById(
+			"question-words"
+		);
+	promptEl.textContent =
+		currentQuestion.prompt;
+	choicesEl.innerHTML = "";
+	currentQuestion.options.forEach(
+		function (choice, i) {
+			let choiceBtn =
+				document.createElement(
+					"button"
+				);
+			choiceBtn.setAttribute(
+				"value",
+				choice
+			);
+			choiceBtn.textContent =
+				i + 1 + ". " + choice;
+			choiceBtn.onclick =
+				questionClick;
+			choicesEl.appendChild(
+				choiceBtn
+			);
+		}
+	);
+}
 
-// Selects element by class
-var timeEl = document.querySelector(".time");
+// Check for right answers and deduct
+// Time for wrong answer, go to next question
 
-// Selects element by id
-var mainEl = document.getElementById("main");
+function questionClick() {
+	if (
+		this.value !==
+		questions[currentQuestionIndex]
+			.answer
+	) {
+		time -= 10;
+		if (time < 0) {
+			time = 0;
+		}
+		timerEl.textContent = time;
+		feedbackEl.textContent = `Wrong! The correct answer was 
+		${questions[currentQuestionIndex].answer}.`;
+		feedbackEl.style.color = "red";
+	} else {
+		feedbackEl.textContent =
+			"Correct!";
+		feedbackEl.style.color =
+			"green";
+	}
+	feedbackEl.setAttribute(
+		"class",
+		"feedback"
+	);
+	setTimeout(function () {
+		feedbackEl.setAttribute(
+			"class",
+			"feedback hide"
+		);
+	}, 2000);
+	currentQuestionIndex++;
+	if (
+		currentQuestionIndex ===
+		questions.length
+	) {
+		quizEnd();
+	} else {
+		getQuestion();
+	}
+}
 
-var secondsLeft = 3;
+// End quiz by hiding questions,
+// Stop timer and show final score
 
-function setTime() {
-  // Sets interval in variable, this is saying every milisecond, run this 
-  //function until it is cleared
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " til end of quiz.";
+function quizEnd() {
+	clearInterval(timerId);
+	let endScreenEl =
+		document.getElementById(
+			"quiz-end"
+		);
+	endScreenEl.removeAttribute(
+		"class"
+	);
+	let finalScoreEl =
+		document.getElementById(
+			"score-final"
+		);
+	finalScoreEl.textContent = time;
+	questionsEl.setAttribute(
+		"class",
+		"hide"
+	);
+}
 
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
-    }
+// End quiz if timer reaches 0
 
-  }, 1000);
-};
+function clockTick() {
+	time--;
+	timerEl.textContent = time;
+	if (time <= 0) {
+		quizEnd();
+	}
+}
+
+// Save score in local storage
+// Along with users' name
+
+function saveHighscore() {
+	let name = nameEl.value.trim();
+	if (name !== "") {
+		let highscores =
+			JSON.parse(
+				window.localStorage.getItem(
+					"highscores"
+				)
+			) || [];
+		let newScore = {
+			score: time,
+			name: name,
+		};
+		highscores.push(newScore);
+		window.localStorage.setItem(
+			"highscores",
+			JSON.stringify(highscores)
+		);
+		alert(
+			"Your Score has been Submitted"
+		);
+	}
+}
+
+// Save users' score after pressing enter
+
+function checkForEnter(event) {
+	if (event.key === "Enter") {
+		saveHighscore();
+		alert(
+			"Your Score has been Submitted"
+		);
+	}
+}
+nameEl.onkeyup = checkForEnter;
+
+// Save users' score after clicking submit
+
+submitBtn.onclick = saveHighscore;
+
+// Start quiz after clicking start quiz
+
+startQuizBtn.onclick = quizStart;
 
 
 
-// Function to create and append colorsplosion image
-function sendMessage() {
-    var submitMessage = "Time's up!"
 
-    var textNode = document.createTextNode(submitMessage)
-    mainEl.appendChild(textNode);
-};
 
-setTime();
+
+
+// //Create questions array
+// const questions = [
+//     "Question 1: What is not one of the fundamentals of Javascript?",
+//     "Question 2: Which HTML element do we put the Javascript inside of?",
+//     "Question 3: How do you create a function in Javascript"
+// ]
+
+// const startTestBtn = document.getElementById('startTestBtn');
+// const questionContainer = document.getElementById('questionContainer');
+// let currentQuestionIndex = 0;
+
+// function showNextQuestion() {
+//     if (currentQuestionIndex < questions.length) {
+//         questionContainer.textContent = questions[currentQuestionIndex];
+//         currentQuestionIndex++;
+//     } else {
+//         questionContainer.textContent = "End of Quiz";
+//     }
+// }
+
+// startTestBtn.addEventListener ('click', () => {
+//     showNextQuestion();
+// });
+
+// // Selects element by class
+// var timeEl = document.querySelector(".time");
+
+// // Selects element by id
+// var mainEl = document.getElementById("main");
+
+// var secondsLeft = 3;
+
+// function setTime() {
+//   // Sets interval in variable, this is saying every milisecond, run this 
+//   //function until it is cleared
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " til end of quiz.";
+
+//     if(secondsLeft === 0) {
+//       // Stops execution of action at set interval
+//       clearInterval(timerInterval);
+//       // Calls function to create and append image
+//       sendMessage();
+//     }
+
+//   }, 1000);
+// };
+
+
+
+// // Function to create and append colorsplosion image
+// function sendMessage() {
+//     var submitMessage = "Time's up!"
+
+//     var textNode = document.createTextNode(submitMessage)
+//     mainEl.appendChild(textNode);
+// };
+
+// setTime();
